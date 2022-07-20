3fb684953c0dc1d0b23451f8b4a0e136eb4e8b3a
Nathanael Kevin Aryatama <106598092+natkevin@users.noreply.github.com>
2022-07-20 19:12:55 +0700

    Update real_number.lua
    add commet of method

--- real_number.lua
+++ real_number.lua
@@ -1,4 +1,12 @@
 function check_real_number_02(number)
+    --[[
+        The method for checking the real number is POSITVE/NEGATIVE
+        INPUT:
+          - number : numeric
+        OUPUT:
+          - information : string
+    ]]
+    
     return_msg = 'The Positive Number'
     if (number < 0) then
         return_msg = 'The Negative Number';
