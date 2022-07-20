f5a7a19f8b26ff782c4a2e6ab8bbf9058d24d915
Nathanael Kevin Aryatama <106598092+natkevin@users.noreply.github.com>
2022-07-20 19:09:38 +0700

    Update real_number.go
    add comment of method description

--- real_number.go
+++ real_number.go
@@ -1,10 +1,16 @@
-// You can edit this code!
-// Click here and start typing.
 package main
 
 import "fmt"
 
 func check_real_number_01(number int) string {
+	/*
+	The method for checking the real number is POSITVE/NEGATIVE
+	INPUT:
+		- number : numeric
+	OUPUT:
+		- information : string
+	*/
+
 	var msg = "The Positive Number"
 	if number < 0 {
 		msg = "The Negative Number"
