823773fbd74f4a11c816ebfefa8a88a4d3ce4aee
NatKevin <nathankevin0810@gmail.com>
2022-07-24 18:30:21 +0700

    generate real number py

---
+++ pythons/real_number.py
@@ -0,0 +1,29 @@
+def check_real_number_01(number):
+    """
+    The method for checking the real number is POSITVE/NEGATIVE
+    INPUT:
+      - number : numeric
+    OUPUT:
+      - information : string
+    """
+    
+    if (number >= 0):
+        return 'The Positive Number'
+    else:
+        return 'The Negative Number'
+      
+      
+def check_real_number_02(number):
+    """
+    The method for checking the real number is POSITVE/NEGATIVE
+    INPUT:
+      - number : numeric
+    OUPUT:
+      - information : string
+    """
+    
+    return_msg = 'The Positive Number'
+    if (number < 0):
+        return_msg = 'The Negative Number'
+    
+    return return_msg
