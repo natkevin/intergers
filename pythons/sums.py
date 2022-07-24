104d2faacadaa83643d368c25c1434d3623ce40c
Nathanael Kevin Aryatama <106598092+natkevin@users.noreply.github.com>
2022-07-24 18:56:38 +0700

    Create sums.py

---
+++ pythons/sums.py
@@ -0,0 +1,12 @@
+# Method as Function of SUM 2 integers
+def sum(a, b):
+    return (a + b)
+
+# Input a as First Number
+a = int(input('Enter 1st number: '))
+
+# Input b as Second Number
+b = int(input('Enter 2nd number: '))
+
+# Display the result with call a method sum
+print(f'Sum of {a} and {b} is {sum(a, b)}')
