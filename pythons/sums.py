e399d62d7a0a8cd293df446f3cfa960b636d26e8
Nathanael Kevin Aryatama <106598092+natkevin@users.noreply.github.com>
2022-07-24 20:02:52 +0700

    Update sums.py

--- pythons/sums.py
+++ pythons/sums.py
@@ -10,3 +10,50 @@ b = int(input('Enter 2nd number: '))
 
 # Display the result with call a method sum
 print(f'Sum of {a} and {b} is {sum(a, b)}')
+
+
+
+# Method as Function of substract 2 integers
+def substract(a, b):
+    return (a - b)
+
+# Input a as First Number
+a = int(input('Enter 1st number: '))
+
+# Input b as Second Number
+b = int(input('Enter 2nd number: '))
+
+# Display the result with call a method substract
+print(f'Substract of {a} and {b} is {substract(a, b)}')
+
+
+
+
+# Method as Function of division 2 integers
+def division(a, b):
+    return (a / b)
+
+# Input a as First Number
+a = int(input('Enter 1st number: '))
+
+# Input b as Second Number
+b = int(input('Enter 2nd number: '))
+
+# Display the result with call a method division
+print(f'Division of {a} and {b} is {division(a, b)}')
+
+
+
+
+# Method as Function of multiplication 2 integers
+def multiplication(a, b):
+    return (a * b)
+
+# Input a as First Number
+a = int(input('Enter 1st number: '))
+
+# Input b as Second Number
+b = int(input('Enter 2nd number: '))
+
+# Display the result with call a method multiplication
+print(f'Multiplication of {a} and {b} is {multiplication(a, b)}')
