ca7b1af0496753160d3486056f370bd83633ec03
Nathanael Kevin Aryatama <106598092+natkevin@users.noreply.github.com>
2022-08-17 19:57:35 +0700

    Update sums.go
    add float 64

--- golangs/sums.go
+++ golangs/sums.go
@@ -17,3 +17,25 @@ func main() {
 	// Display result
 	fmt.Print("The Sum of a and num2  = ", sum(a, b))
 }
+//float64
+
+package main
+
+import (
+	"fmt"
+)
+
+/* Function Sum */
+func sum(a, b float64) float64 {
+	return (a + b)
+}
+
+func main() {
+	// Set a and b
+	a := 1.9
+	b := 1.9
+
+	// Display result
+	fmt.Print("The Sum of a and num2  = ", sum(a, b))
+}
+
