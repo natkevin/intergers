378650cea1856faf9d94c76be1bfd08fbc3ca5b3
Nathanael Kevin Aryatama <106598092+natkevin@users.noreply.github.com>
2023-01-08 19:42:28 +0700

    Update funtions.py
    create a calculate around function

--- pythons/funtions.py
+++ pythons/funtions.py
@@ -1,1 +1,34 @@
-:
+# Online Python - IDE, Editor, Compiler, Interpreter
+def calculate_round_square(length_x, length_y=90):
+    """Function to calculate a round
+    - Input 
+      -- length_x as Numeric
+      -- length_y as Numeric with default value = 90
+    - Ouput:
+      -- around = 2*(length_x + length_y)
+    """
+    around = 2*(length_x + length_y)
+    return around 
+    
+
+
+if __name__ == "__main__":
+    # length array used tupple
+    ls = (10,20,30,40,50,60)
+    
+    # calculate around base ls 
+    arounds = []
+    for l in ls:
+        print("Length : %s" % (l,))
+        # calculate_round_square with length_x=l and length_y=90
+        a = calculate_round_square(l)
+        print("Around : %s" % (a,))
+        
+        # add around a to arounds array 
+        arounds.append(a)
+        
+        
+    print("Length row :") 
+    print(ls)
+    print("Arounds row :")
+    print(arounds)
